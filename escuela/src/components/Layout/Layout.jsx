5f1dec12c0ca441a21e57ac66dd92fc05574cb05
Josecfr11 <josecfr11@gmail.com>
2025-01-20 20:28:33 -0700

    ADD metodologia BEM

--- escuela/src/components/Layout/Layout.jsx
+++ escuela/src/components/Layout/Layout.jsx
@@ -3,21 +3,20 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from '../Layout/Layout.module.css';
 import Aside from '../Aside/Aside'
-import Toast from '../Toast/Toast';
 
 function Layout() {
     return (
         <div className={styles['container-layout']}>
             <Aside></Aside>
-            <div className={styles['container-page']}>
-                <header className={styles['header']}>
+            <div className={styles['container-layout__container-page']}>
+                <header className={styles['container-page__header']}>
                     <h1>Escuela</h1>
                 </header>
 
-                <main className={styles['page']}>
+                <main className={styles['container-page__page']}>
                     <Outlet />
                 </main>
-                <footer className={styles['footer']}>
+                <footer className={styles['container-page__footer']}>
                     <p>&copy; 2025 Mi Aplicación</p>
                 </footer>
             </div>
